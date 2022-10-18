5ae1c14ae47b103c31033bff36cceeae872976fa
cperezgo <christianperezglez@outlook.com>
2022-10-18 19:35:06 +0200

    comienzo de backend springboot

---
+++ backend/src/main/java/es/uniovi/tfm/epivector/EpivectorApplication.java
@@ -0,0 +1,13 @@
+package es.uniovi.tfm.epivector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpivectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpivectorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/es/uniovi/tfm/epivector/EpivectorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uniovi.tfm.epivector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpivectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
